b5a1c1a8afdde2dfd6c3861653dadc0a92826d28
TeseySTD <skoreiko.mykhailo@chnu.edu.ua>
2024-09-24 09:54:14 +0300

    Ended users-app

---
+++ Lab5/users-app/src/components/UserCard.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="user-card" :class="{ adult: userData.age > 18, minor: userData.age <= 18 }">
+        <img :src="userData.photo" alt="User Photo" class="user-photo" />
+        <h2>{{ userData.firstName }} {{ userData.lastName }}</h2>
+        <p>{{ userData.position }}</p>
+        <p>Gender: {{ userData.gender }}</p>
+        <p v-if="userData.age > 18">Age: {{ userData.age }}</p>
+        <ul>
+            <li v-for="(hobby, index) in userData.hobbies" :key="index">{{ hobby }}</li>
+        </ul>
+    </div>
+</template>
+
+<script lang="ts" setup>
+    import type { UserData } from '@/ts/user_data'
+    const props = defineProps<{ userData: UserData }>()
+</script>
+
+<style scoped>
+.user-card {
+        border: 1px solid #ccc;
+        border-radius: 8px;
+        padding: 16px;
+        margin: 10px;
+        text-align: center;
+        color: #373636;
+    }
+    .user-card img {
+        width: 100px;
+        border-radius: 50%;
+    }
+    .adult {
+        background-color: hsla(160, 100%, 37%, 1);
+    }
+    .minor {
+        background-color: lightgreen;
+    }
+</style>
---
+++ Lab5/users-app/src/components/UsersList.vue
@@ -0,0 +1,157 @@
+<template>
+    <div id="users-list">
+        <div class="toolbar">
+            <button @click="filterGender('male')">Male</button>
+            <button @click="filterGender('female')">Female</button>
+            <button @click="resetFilter">Show All</button>
+        </div>
+
+        <div v-if="filteredUsers.length > 0" class="user-list">
+            <UserCard v-for="(user, index) in filteredUsers" :key="index" :userData="user" />
+        </div>
+        <p v-else>Список юзерів пустий</p>
+    </div>
+</template>
+
+<script lang="ts" setup>
+    import { ref, computed } from 'vue'
+    import UserCard from './UserCard.vue'
+
+    const users = ref([
+        {
+            firstName: 'John',
+            lastName: 'Doe',
+            gender: 'male',
+            age: 25,
+            position: 'Developer',
+            photo: 'https://via.placeholder.com/150',
+            hobbies: ['Reading', 'Coding']
+        },
+        {
+            firstName: 'Jane',
+            lastName: 'Smith',
+            gender: 'female',
+            age: 15,
+            position: 'Designer',
+            photo: 'https://via.placeholder.com/150',
+            hobbies: ['Art', 'Photography']
+        },
+        {
+            firstName: 'Bob',
+            lastName: 'Smith',
+            gender: 'male',
+            age: 25,
+            position: 'Manager',
+            photo: 'https://via.placeholder.com/150',
+            hobbies: ['Reading', 'Coding']
+        },
+        {
+            firstName: 'Alice',
+            lastName: 'Johnson',
+            gender: 'female',
+            age: 17,
+            position: 'Teacher',
+            photo: 'https://via.placeholder.com/150',
+            hobbies: ['Reading', 'Photography']
+        },
+        {
+            firstName: 'John',
+            lastName: 'Williams',
+            gender: 'male',
+            age: 17,
+            position: 'Developer',
+            photo: 'https://via.placeholder.com/150',
+            hobbies: ['Reading', 'Coding']
+        },
+        {
+            firstName: 'Mary',
+            lastName: 'Brown',
+            gender: 'female',
+            age: 25,
+            position: 'Engineer',
+            photo: 'https://via.placeholder.com/150',
+            hobbies: ['Reading', 'Photography']
+        },
+        {
+            firstName: 'David',
+            lastName: 'Miller',
+            gender: 'male',
+            age: 25,
+            position: 'Manager',
+            photo: 'https://via.placeholder.com/150',
+            hobbies: ['Reading', 'Coding']
+        },
+        {
+            firstName: 'Sarah',
+            lastName: 'Davis',
+            gender: 'female',
+            age: 25,
+            position: 'Teacher',
+            photo: 'https://via.placeholder.com/150',
+            hobbies: ['Reading', 'Photography']
+        },
+        {
+            firstName: 'David',
+            lastName: 'Miller',
+            gender: 'male',
+            age: 25,
+            position: 'Manager',
+            photo: 'https://via.placeholder.com/150',
+            hobbies: ['Reading', 'Coding']
+        },
+        {
+            firstName: 'Sarah',
+            lastName: 'Davis',
+            gender: 'female',
+            age: 25,
+            position: 'Teacher',
+            photo: 'https://via.placeholder.com/150',
+            hobbies: ['Reading', 'Photography']
+        }
+    ])
+
+    const genderFilter = ref<string | null>(null)
+
+    const filteredUsers = computed(() => {
+        if (!genderFilter.value) {
+            return users.value
+        }
+        return users.value.filter((user) => user.gender === genderFilter.value)
+    })
+
+    function filterGender(gender: string) {
+        genderFilter.value = gender
+    }
+
+    function resetFilter() {
+        genderFilter.value = null
+    }
+</script>
+
+<style scoped>
+    #users-list {
+        display: flex;
+        flex-direction: column;
+        justify-content: left;
+    }
+    .toolbar {
+        display: block;
+        justify-content: space-between;
+        margin-bottom: 10px;
+        max-width: 200px;
+    }
+    .toolbar > button {
+        margin: 5px;
+        background-color: hsla(160, 100%, 37%, 1);
+        border: solid 3px #4c4c4c;
+    }
+    .toolbar > button:hover {
+        background-color: lightgreen;
+    }
+    .user-list {
+        display: grid;
+        grid-template-columns: 200px 200px 200px 200px 200px;
+        grid-template-rows: auto;
+        background-color: #333333;
+    }
+</style>
